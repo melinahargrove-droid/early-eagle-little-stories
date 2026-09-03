82aabd02d1209a000bf07301de5509ac886fb550
melinahargrove-droid <melina.hargrove@gmail.com>
2026-09-02 19:54:44 -0500

    Load reliable Home fade styling

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './AppClean.jsx'
 import BackupDock from './features/backup/BackupDock.jsx'
 import './app-clean.css'
+import './home-fade-fix.css'
 import './features/backup/backup.css'
 
 createRoot(document.getElementById('root')).render(
