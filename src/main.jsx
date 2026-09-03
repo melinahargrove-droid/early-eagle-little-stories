0f6078ef0360f5672dfd8fe4a0b7586a14d72b93
melinahargrove-droid <melina.hargrove@gmail.com>
2026-09-02 21:31:59 -0500

    Load Everyday Classroom cover styles

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './AppClean.jsx'
 import BackupDock from './features/backup/BackupDock.jsx'
 import './app-clean.css'
 import './home-fade-fix.css'
+import './everyday-cover.css'
 import './features/backup/backup.css'
 
 createRoot(document.getElementById('root')).render(
