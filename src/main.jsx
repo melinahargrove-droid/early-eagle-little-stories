5c1bf679c3ac4c402a50cdb539fcbe6ebec7511f
melinahargrove-droid <melina.hargrove@gmail.com>
2026-09-04 13:42:08 -0500

    Remove floating backup cloud and load mobile workflow fixes

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './AppClean.jsx'
-import BackupDock from './features/backup/BackupDock.jsx'
 import './app-clean.css'
 import './asset-crops.css'
+import './ux-fixes.css'
 import './home-fade-fix.css'
 import './everyday-cover.css'
 import './features/backup/backup.css'
@@ -11,6 +11,5 @@ import './features/backup/backup.css'
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <BackupDock />
   </React.StrictMode>,
 )
