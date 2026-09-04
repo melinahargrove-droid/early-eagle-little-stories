d3cbe3df223adfc030790868a0f6f53f19ebb01b
melinahargrove-droid <melina.hargrove@gmail.com>
2026-09-04 17:06:16 -0500

    Enable automatic book safety and cloud recovery checks

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './AppClean.jsx'
+import DataSafetyGuard from './features/safety/DataSafetyGuard.jsx'
 import './app-clean.css'
 import './asset-crops.css'
 import './ux-fixes.css'
 import './home-fade-fix.css'
 import './everyday-cover.css'
 import './features/backup/backup.css'
+import './features/safety/data-safety.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <DataSafetyGuard />
   </React.StrictMode>,
 )
